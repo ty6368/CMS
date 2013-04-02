bfea801d8f4eb84d49144951ca36353f49679b91
Tim Young <ty6368@att.com>
2013-04-01 21:55:58 -0700

    Update do_say.rb

--- do_say.rb
+++ do_say.rb
@@ -1,7 +1,9 @@
 callerID = $currentCall.callerID
 log 'Incoming call from: ' + callerID
-if callerID == '4075550100'
+if callerID == '4257850159'
+log 'REJECTED YOU TIM !!!!!!!!!!!!!!!!!!!!"
     reject
 else
+log 'DID NOT reject you TIM at 1717 !!!!!!!!!!!!!!!!!!!!"
     say "You passed the test!"
 end

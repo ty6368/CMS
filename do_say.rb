1402d088bff93709efe0ddd6ac0dc1f59ac2d04f
Tim Young <ty6368@att.com>
2013-04-01 22:38:42 -0700

    Update do_say.rb

--- do_say.rb
+++ do_say.rb
@@ -1,11 +1,13 @@
 callerID = $currentCall.callerID
 log '********************************************************** Incoming call from: ' + callerID
 if callerID == '4257850159'
-log '==================== REJECTED YOU TIM ===================='
-    reject
+log '==================== REJECTED SINCE YOU CALLED FROM TIMs PHONE ===================='
+    message "You have called from Tims phone. This is the IF, branch of this test code.", {
+       :to => "+14255801717", 
+       :network => 'PSTN'}
 else
 log '!!!!!!!!!!!!!!!!!!!!!! DID NOT reject you TIM at 1717 !!!!!!!!!!!!!!!!!!!!'
-    message "Received office voice mail.", {
+    message "You did not call from Tims phone. This is the else, branch of this test code.", {
        :to => "+14255801717", 
        :network => 'PSTN'}
 end

8e641af727472da10eee10641efffaef3d6abdfe
Tim Young <ty6368@att.com>
2013-04-01 21:29:07 -0700

    Update do_say2.js

--- do_say2.js
+++ do_say2.js
@@ -1,8 +1,2 @@
-var callerID = currentCall.callerID;
-log("Incoming call from: " + callerID);
-if (callerID == "4075550100") 
-    {
-    reject();
-    }
-else
-    say("You passed the test!");
+say("Welcome to Tropo!");
+log("Caller ID: " + currentCall.callerID);

7f7d1f1ad2f0e17cbdcacd12322dcb2c092952eb
Tim Young <ty6368@att.com>
2013-04-01 23:15:57 -0700

    Update do_say.rb

--- do_say.rb
+++ do_say.rb
@@ -1,18 +1,17 @@
 callerID = $currentCall.callerID
 log '********************************************************** Incoming call from: ' + callerID
 if callerID == '+14257850159'
-log '==================== REJECTED SINCE YOU CALLED FROM TIMs PHONE ===================='
+log '11111111111111111111111111111111111111111111111111111111111'
     #say "if branch if branch if branch"
     message "You have called from Tims phone. . . You have called from Tims phone.", {
        :to => "+14255801717", 
        :network => 'PSTN'}
        hangup
 else
-log '!!!!!!!!!!!!!!!!!!!!!! DID NOT reject you TIM at 1717 !!!!!!!!!!!!!!!!!!!!'
-    say "else branch else branch else branch"
+log '222222222222222222222222222222222222222222222222222222222222'
+    #say "else branch else branch else branch"
     message "You did not call from Tims phone. . . You did not call from Tims phone", {
        :to => "+14255801717", 
        :network => 'PSTN'}
        hangup
 end
-hangup

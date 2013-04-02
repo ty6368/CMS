572ab8ae24f11ef6ef2ee536a969e95500f4625f
Tim Young <ty6368@att.com>
2013-04-01 21:14:38 -0700

    Update do_say2.js

--- do_say2.js
+++ do_say2.js
@@ -1,4 +1,13 @@
-message("Received office voice mail", {
-    to:"+14255801717",
-    network:"SMS"
+ask("What's your four or five digit pin? Press pound when finished", {
+    choices:"[4-5 DIGITS]",
+    terminator:"#",
+    timeout:15.0,
+    mode:"dtmf",
+    interdigitTimeout: 5,
+    onChoice: function(event) {
+        say("On Choice!");
+    },
+    onBadChoice: function(event) {
+        say("On Bad Choice!");
+    }
 });

15ac73c7941d8b1741c3df1bc99af6b9e212f7a6
Tim Young <ty6368@att.com>
2013-04-01 23:10:01 -0700

    Update do_say.rb

--- do_say.rb
+++ do_say.rb
@@ -9,7 +9,7 @@ log '==================== REJECTED SINCE YOU CALLED FROM TIMs PHONE ============
        hangup
 else
 log '!!!!!!!!!!!!!!!!!!!!!! DID NOT reject you TIM at 1717 !!!!!!!!!!!!!!!!!!!!'
-    #say "else branch else branch else branch"
+    say "else branch else branch else branch"
     message "You did not call from Tims phone. . . You did not call from Tims phone", {
        :to => "+14255801717", 
        :network => 'PSTN'}
